236252412658d16ca8c61b2b4f3d0392fbf499b1
arnaldo dominguez <arnaldodt@gmail.com>
2022-11-10 17:00:19 -0300

    Error, se sube el codigo correcto

--- script1.ts
+++ script1.ts
@@ -1,15 +1,39 @@
-
-var myString: string;
-// Puedo asignar mySring así:
-myString = "Bee stinger";
-// ¿Por qué hay problema con esto? ¿Qué puedo hacer para arreglarlo?
-//myString = 9;
-myString = "9";
-
-/*
-
-El error se genera en la linea 5
-myString esta definido como string y le estamos asignando un numeric
-la solucion esta en la linea 7
-
-*/
+class bike{
+    public millas:number
+ 
+    constructor(public precio: number,public velocidad_max:string){
+       this.millas=0;
+    }
+    public displayInfo = ()=>{
+       console.log(this.precio, this.velocidad_max, Math.abs(this.millas));
+    }
+    public ride = () => {
+       console.log("Riding");
+       this.millas +=10;
+    }
+    public reverse = () => {
+       console.log("Reversing");
+       this.millas -=5;
+    }
+ }
+ console.log('ejecucion')
+ const bike1 = new bike(200,"25mph");
+ bike1.ride();
+ bike1.ride();
+ bike1.ride();
+ bike1.reverse();
+ bike1.displayInfo();
+ 
+ const bike2 = new bike(520,"50mph");
+ bike2.ride();
+ bike2.ride();
+ bike2.reverse();
+ bike2.reverse();
+ bike2.displayInfo();
+ 
+ const bike3 = new bike(650,"90mph");
+ bike3.reverse();
+ bike3.reverse();
+ bike3.reverse();
+ bike3.displayInfo();
+ 
